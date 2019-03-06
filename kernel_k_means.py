a6d9f4e345397b6e752221f0a38c257cfd51ec64
Morteza <Morteza.j8@gmail.com>
2019-03-06 17:25:50 +0330

    k means on two moon data set

--- kernel_k_means.py
+++ kernel_k_means.py
@@ -6,24 +6,39 @@ import sklearn.cluster as cl
 import numpy as np
 import itertools
 
-matplotlib.style.use('ggplot') #makes plots look pretty
 
 # Generate a dataset and plot it
-x, y = sklearn.datasets.make_moons(50, 0, noise=0.1)
-xx, yy = sklearn.datasets.make_circles(60, 0, noise=0.08 , factor=0.1)
+two_moon_x, two_moon_y = sklearn.datasets.make_moons(500, 0, noise=0.035)
+# two_ring_x, two_ring_y = sklearn.datasets.make_circles(500, 0, noise=0.15, factor=0.2)
 
 
-
-plt.scatter(xx[:,0], xx[:,1], s=25, c=yy, cmap=plt.cm.Spectral, marker='o' )
-plt.scatter(x[:,0], x[:,1], s=50, c=y, cmap=plt.cm.Spectral , marker='x')
+# plt.scatter(two_ring_x[:, 0], two_ring_x[:, 1], s=25, c=two_ring_y, cmap=plt.cm.Spectral, marker='o')
+# plt.scatter(two_moon_x[:, 0], two_moon_x[:, 1], s=50, c=two_moon_y, cmap=plt.cm.Spectral, marker='x')
 
 # data = np.arra
 
-# kmeans = cl.KMeans(n_clusters=2, random_state=0).fit(x)
 
 
 
+#[ [1,  2], [1,  4], [1,  0],
+#  [10, 2], [10, 4], [10, 0]
+#]
+
+
+# for u in range(0 , len(two_moon_x))
+
+
+
+
+kmeans = cl.KMeans(n_clusters=2, random_state=0).fit(two_moon_x)
+labels = kmeans.labels_
+predicate = kmeans.predict([[0, 0], [12, 3]])
+cl_center = kmeans.cluster_centers_
+
+matplotlib.style.use('ggplot') #makes plots look pretty
+plt.scatter(two_moon_x[:,0],two_moon_x[:,1], s = 30 , c = labels, cmap=plt.cm.Spectral)
 plt.show()
+
 
 
 
